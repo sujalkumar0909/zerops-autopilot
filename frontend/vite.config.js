d843dc00e5d710807a61f81ecadb689ab70beecc
Sujal <sujalkumar7209@gmail.com>
2026-08-08 22:07:30 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+
   preview: {
-  allowedHosts: ["frontend-2b79-3000.prg1.zerops.app"],
-},
+    allowedHosts: true,
+  },
 })
